231a3e5aa907d96615cb20a1ca2688eda1c5a6da
Digvijay Singh Shekhawat <djdigvijay69@gmail.com>
2015-10-25 14:12:11 +0530

    with theme

--- app/src/main/java/com/devilcity/messdl/WednesdayBreakfast.java
+++ app/src/main/java/com/devilcity/messdl/WednesdayBreakfast.java
@@ -57,7 +57,7 @@ public class WednesdayBreakfast extends ActionBarActivity {
         // longer or shorter and speed
         private static final int SWIPE_MIN_DISTANCE = 120;
         private static final int SWIPE_MAX_OFF_PATH = 200;
-        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 100;
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2,
